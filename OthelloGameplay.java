5a4711b04a68d9a6689f7b124ae3cca642a5d4da
jsomar08 <mohammedomar6498@gmail.com>
2026-01-06 10:53:32 -0500

    Update OthelloGameplay.java

--- OthelloGameplay.java
+++ OthelloGameplay.java
@@ -1,3 +1,26 @@
-class OthelloGameplay {
-    
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class OthelloGame extends JFrame {
+
+    JPanel main;
+    JLabel statusLabel;
+    Board board;
+    Player blackPlayer;
+    Player whitePlayer;
+    Player currentPlayer;
+
+
+        public OthelloGame() {
+        super("Othello");
+        setUpGame();
+        setUpPanels();
+        setUpBoardButtons();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
 }
+
