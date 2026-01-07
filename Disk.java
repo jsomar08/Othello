197bb567717ecc1afb94ac5fcfd534fcab4a9812
T-JBygnauth6741 <781644@pdsb.net>
2026-01-07 09:28:39 -0500

    Add color property and methods to Disk class

--- Disk.java
+++ Disk.java
@@ -1,3 +1,31 @@
+
 class Disk {
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Disk(String color) {
+        this.color = color;
+    }
+
+    public static String flipingPlayer(String color){
+        if(color.equals( "•")){
+            color = "○";
+        }
+        else if(color.equals( "○")){
+            color = "•";
+        }
+        return color;
+    }
+
+
+
+
 
 }
