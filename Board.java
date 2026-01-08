02ae378e4ae0cfc9fd7c5b263e00ea5ee9ed75ac
jsomar08 <mohammedomar6498@gmail.com>
2026-01-08 10:08:25 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -27,13 +27,7 @@ public class Board {
         }
         System.out.println();
 
-        for (int r = 0; r < 8; r++) {
-            System.out.print((char)('A' + r) + " ");
-            for (int c = 0; c < 8; c++) {
-                System.out.print(board[r][c].getColor() + " ");
-            }
-            System.out.println();
-        }
+        
     }
 
     public boolean isInsideBoard(int row, int col) {
@@ -56,7 +50,7 @@ public class Board {
         return count;
     }
 
-    public int[] convertRowToLetter(String i) { // try to interpret A5 as row 1 column 5
+    public int[] convertRowToLetter(String i) { // try to interpret for example A5 as row 1 column 5
 
 
         
