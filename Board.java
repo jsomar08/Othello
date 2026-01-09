3dc7a797109597d102bd5c294ab4a27ca68b16b5
T-JBygnauth6741 <781644@pdsb.net>
2026-01-09 07:37:26 -0500

    Update Board.java
    Updated method signatures to include comments explaining their purpose.

--- Board.java
+++ Board.java
@@ -7,7 +7,7 @@ public class Board {
         initializeBoard();
     }
 
-    public void initializeBoard() {
+    public void initializeBoard() {//creates the board
         for (int r = 0; r < 8; r++) {
             for (int c = 0; c < 8; c++) {
                 board[r][c] = new Disk(".");
@@ -20,25 +20,31 @@ public class Board {
         board[4][4] = new Disk("•");
     }
 
-    public void printBoard() {
+    public void printBoard() {//prints board layout
         System.out.print("  ");
         for (int c = 1; c <= 8; c++) {
             System.out.print(c + " ");
         }
         System.out.println();
 
-        
+        for (int r = 0; r < 8; r++) {
+            System.out.print((char)('A' + r) + " ");
+            for (int c = 0; c < 8; c++) {
+                System.out.print(board[r][c].getColor() + " ");
+            }
+            System.out.println();
+        }
     }
 
     public boolean isInsideBoard(int row, int col) {
         return row >= 0 && row < 8 && col >= 0 && col < 8;
     }
 
-    public boolean isEmptyPosition(int row, int col) {
+    public boolean isEmptyPosition(int row, int col) {//if the board pellet is empty 
         return isInsideBoard(row, col) && board[row][col].isEmpty();
     }
-
-    public int countDisks(String color) {
+    
+    public int countDisks(String color) {// counts the amount of disks
         int count = 0;
         for (int r = 0; r < 8; r++) {
             for (int c = 0; c < 8; c++) {
@@ -50,13 +56,13 @@ public class Board {
         return count;
     }
 
-    public int[] convertRowToLetter(String i) { // try to interpret for example A5 as row 1 column 5
-
-
-        
+    public void rowNumberToLetter(int row, int col){// try to interpret A5 as row 1 column 5
+        char[] letters = {'A','B','C','D','E','F','G','H'};
+        System.out.println(letters[row - 1] + col);
+    
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// testing board
         Board b = new Board();
         b.printBoard();
     }
