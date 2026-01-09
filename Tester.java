cda801e3416464f6aa34f00e8302c68294446c64
T-JBygnauth6741 <781644@pdsb.net>
2026-01-09 07:38:25 -0500

    Add main method to Tester class

--- Tester.java
+++ Tester.java
@@ -1,3 +1,9 @@
 public class Tester {
-    
+    public static void main(String[] args) {
+        Disk testBlack = new Disk("○");
+        Disk testWhite = new Disk("•");
+        System.out.println(testBlack.flipingPlayer());
+        System.out.println("hello");
+        System.out.println(testWhite.flipingPlayer());
+    }
 }
