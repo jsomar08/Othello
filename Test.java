0c7464f5e406890d04a0812554ea207d9ab4eec7
jsomar08 <mohammedomar6498@gmail.com>
2026-01-19 21:30:54 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,49 +1,41 @@
-class BoardTest{
-    public static void otherMethods(String[] args) {// testing board
+class BoardTest {
+    public static void testOtherMethods() {
         int row1 = 1;
         int col1 = 5;
         int row2 = 2;
         int col2 = 5;
-        String coord1 = "A5";
-        String coord2 = "D3";
+        
         Board b = new Board();
         b.printBoard();
-        System.out.println(b.isInsideBoard(row1,col1));
-        System.out.println(b.letterToRowNumber(coord1));
-        System.out.println(b.canFlipAll(row1, col1, b.getAtDiskLocation(row1,col1).getColor()));
-        System.out.println(b.getAtDiskLocation(row1, col1));
-        System.out.println(b.countDisks(b.getAtDiskLocation(row1, col1).getColor()));
-        System.out.println(b.isInsideBoard(row2,col2));
-        System.out.println(b.letterToRowNumber(coord2));
-        System.out.println(b.canFlipAll(row2, col2, b.getAtDiskLocation(row2,col2).getColor()));
-        System.out.println(b.getAtDiskLocation(row2, col2));
-        System.out.println(b.countDisks(b.getAtDiskLocation(row2, col2).getColor()));
         
+        System.out.println("Is inside board (1,5): " + b.isInsideBoard(row1, col1));
+        System.out.println("Disk at (1,5): " + b.getAtDiskLocation(row1, col1).getColor());
+        System.out.println("Count of disks: " + b.countDisks(b.getAtDiskLocation(row1, col1).getColor()));
         
-
-        
-        
-
-
-        
+        System.out.println("Is inside board (2,5): " + b.isInsideBoard(row2, col2));
+        System.out.println("Disk at (2,5): " + b.getAtDiskLocation(row2, col2).getColor());
     }
 
+    public static void main(String[] args) {
+        testOtherMethods();
+    }
+}
 
+class CanFlipTest {
     static void testCanFlip() {
         Board testBoard = new Board();
-        testBoard.initializeBoard(); // Clear board first
+        testBoard.initializeBoard();
         
         Disk white = new Disk("○");
         Disk black = new Disk("•");
 
         // Test 1: Vertical flip UP
-       
-        testBoard.getBoard()[6][5] = new Disk("○"); // starting position
+        testBoard.getBoard()[6][5] = new Disk("○");
         testBoard.getBoard()[5][5] = new Disk("•");
         testBoard.getBoard()[4][5] = new Disk("•");
         testBoard.getBoard()[3][5] = new Disk("•");
         testBoard.getBoard()[2][5] = new Disk("○");
-        System.out.println("Up (should be true): " + testBoard.canFlipUp(6, 5));
+        System.out.println("Up (should be true): " + testBoard.canFlipUp(6, 5, white));
 
         // Test 2: Horizontal flip left
         testBoard = new Board();
@@ -52,7 +44,7 @@ class BoardTest{
         testBoard.getBoard()[5][3] = new Disk("•");
         testBoard.getBoard()[5][2] = new Disk("•");
         testBoard.getBoard()[5][1] = new Disk("○");
-        System.out.println("Left (should be true): " + testBoard.canFlipLeft(5, 5));
+        System.out.println("Left (should be true): " + testBoard.canFlipLeft(5, 5, white));
 
         // Test 3: Diagonal flip left up
         testBoard = new Board();
@@ -61,109 +53,89 @@ class BoardTest{
         testBoard.getBoard()[4][4] = new Disk("•");
         testBoard.getBoard()[3][3] = new Disk("•");
         testBoard.getBoard()[2][2] = new Disk("○");
-        System.out.println("Up-Left (should be true): " + testBoard.canFlipUpLeft(6, 6));
+        System.out.println("Up-Left (should be true): " + testBoard.canFlipUpLeft(6, 6, white));
 
-        // Test 4: Should fail no matching disk at end
+        // Test 4: Should fail  no matching disk at end
         testBoard = new Board();
         testBoard.getBoard()[6][5] = new Disk("○");
         testBoard.getBoard()[5][5] = new Disk("•");
         testBoard.getBoard()[4][5] = new Disk("•");
         testBoard.getBoard()[3][5] = new Disk("•");
-        testBoard.getBoard()[2][5] = new Disk("."); 
-        System.out.println("Up (should be false): " + testBoard.canFlipUp(6, 5));
+        testBoard.getBoard()[2][5] = new Disk(".");
+        System.out.println("Up (should be false): " + testBoard.canFlipUp(6, 5, white));
     }
 
     public static void main(String[] args) {
         testCanFlip();
     }
 }
- 
- 
+
 class DiskTest {
     public static void main(String[] args) {
-        Disk testBlack = new Disk("○");
-        Disk testWhite = new Disk("•");
+        Disk testBlack = new Disk("•");
+        Disk testWhite = new Disk("○");
         Disk testEmpty = new Disk(".");
-        System.out.println(testBlack.flipingPlayer());
-        System.out.println("hello");
-        System.out.println(testWhite.flipingPlayer());
-        System.out.println(testEmpty.flipingPlayer());
-        System.out.println(testBlack.oppositeDisk());
-        System.out.println(testWhite.oppositeDisk());
-        System.out.println(testEmpty.oppositeDisk());
-        System.out.println(testBlack.isEmpty());
-        System.out.println(testWhite.isEmpty());
-        System.out.println(testEmpty.isEmpty());
-
+        
+        System.out.println("Black opposite: " + testBlack.oppositeDisk().getColor());
+        System.out.println("White opposite: " + testWhite.oppositeDisk().getColor());
+        System.out.println("Empty opposite: " + testEmpty.oppositeDisk().getColor());
+        
+        System.out.println("Black is empty: " + testBlack.isEmpty());
+        System.out.println("White is empty: " + testWhite.isEmpty());
+        System.out.println("Empty is empty: " + testEmpty.isEmpty());
     }
-
 }
-class PlayerTest{
-    public static void testGetters() {
+
+class PlayerTest {
+    static void testGetters() {
         Player p1 = new Player("Alice", "•", false);
         Player p2 = new Player("Bot", "○", true);
 
-        System.out.println(p1.getName());
-        System.out.println(p1.getColor());
-        System.out.println(p1.isBot());
+        System.out.println("P1 name: " + p1.getName());
+        System.out.println("P1 color: " + p1.getColor());
+        System.out.println("P1 is bot: " + p1.isBot());
 
-        System.out.println(p2.getName());
-        System.out.println(p2.getColor());
-        System.out.println(p2.isBot());
+        System.out.println("P2 name: " + p2.getName());
+        System.out.println("P2 color: " + p2.getColor());
+        System.out.println("P2 is bot: " + p2.isBot());
     }
+    
     static void testBotMove() {
         Board board = new Board();
         Player bot = new Player("Bot", "○", true);
 
         int[] move = bot.getMove(board);
 
-        System.out.println((move != null));
+        System.out.println("Bot made a move: " + (move != null));
         if (move != null) {
-            System.out.println(move[0] >= 0 && move[0] < 8);
-
-            System.out.println((move[1] >= 0 && move[1] < 8));
+            System.out.println("Row in range: " + (move[0] >= 0 && move[0] < 8));
+            System.out.println("Col in range: " + (move[1] >= 0 && move[1] < 8));
         }
     }
 
     static void testHumanIsBot() {
         Player human = new Player("Human", "•", false);
-
-        System.out.println(!human.isBot());
+        System.out.println("Human is not bot: " + !human.isBot());
     }
 
     public static void main(String[] args) {
-
         testGetters();
         testHumanIsBot();
         testBotMove();
     }
-
 }
 
 class GameTest {
-
-    static void testHasValidMoves() {
-        Game game = new Game();
-
-        Player p1 = new Player("Test1", "•", false);
-        Player p2 = new Player("Test2", "○", false);
-
-        System.out.println(game.hasValidMoves(p1));
-
-        System.out.println(game.hasValidMoves(p2));
-    }
-
-    static void testIsBoardFull() {
-        Game game = new Game();
-
-        System.out.println(game.isBoardFull());
+    static void testGameCreation() {
+        System.out.println("Creating game - follow prompts to test");
+      // This will prompt for user input
+        // Game game = new Game();
+        System.out.println("Game creation test skipped (requires user input)");
     }
 
     public static void main(String[] args) {
-        testHasValidMoves();
-        testIsBoardFull();
+        testGameCreation();
+        System.out.println("\nAll tests require manual verification");
+        System.out.println("Run individual test classes to verify components");
     }
 }
-
-    
-    
